11d6c02933be09ea62967191a59055aff329710f
ellaneves <47819485+ellaneves@users.noreply.github.com>
2019-12-19 22:52:44 +0000

    ok

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -31,24 +31,24 @@ function draw() { //código que corre em loop
    function avatar (posX,posY,raio) {
 
 
-   fill (90,70,60);
-   square (posX-raio/2,posY-raio/1.5, raio/1, 255,255,8,8)
-   square (posX-raio/1.5,posY-raio/2, raio/1, 255,255,8,8) // cabelo
-   square (posX-raio/3,posY-raio/2, raio/1, 255,255,8,8) // cabelo
-  
-   fill(255,202,150)
-   ellipse(posX,posY,raio)
-   fill (255)
-   ellipse (posX-raio/4,posY-raio/10, raio/4, raio/5); //1 -
-   ellipse (posX+raio/4, posY-raio/10,raio/4, raio/5) // 2 + //ellipse (posX-raio/35,posY-raio/40,raio/10)
-   fill (80,180,200)
-   ellipse (posX-raio/4, posY-raio/10, raio/8);
-   ellipse (posX+raio/4, posY-raio/10, raio/8);
-   fill (0)
-   ellipse (posX-raio/4, posY-raio/10, raio/20);
-   ellipse (posX+raio/4, posY-raio/10, raio/20);
+  fill (90,70,60);
+  square (posX-raio/2,posY-raio/1.5, raio/1, 255,255,8,8)
+  square (posX-raio/1.5,posY-raio/2, raio/1, 255,255,8,8) // cabelo
+  square (posX-raio/3,posY-raio/2, raio/1, 255,255,8,8) // cabelo
+
+  fill(255,202,150)
+  ellipse(posX,posY,raio)
+  fill (255)
+  ellipse (posX-raio/4,posY-raio/10, raio/4, raio/5); //1 - olho
+  ellipse (posX+raio/4, posY-raio/10,raio/4, raio/5) // 2 + //ellipse (posX-raio/35,posY-raio/40,raio/10)
+  fill (80,180,200)
+  ellipse (posX-raio/4, posY-raio/10, raio/8); // iris
+  ellipse (posX+raio/4, posY-raio/10, raio/8);
+  fill (0)
+  ellipse (posX-raio/4, posY-raio/10, raio/20); //pupila
+  ellipse (posX+raio/4, posY-raio/10, raio/20);
 
 
-   fill (255, 90, 90);
-   arc(posX-raio/25, posY+raio/5, raio/5, raio/5, 0, -450+ QUARTER_PI, CHORD);// boca
+  fill (255, 90, 90);
+  arc(posX-raio/25, posY+raio/5, raio/5, raio/5, 0, -450+ QUARTER_PI, CHORD);// boca
 }
